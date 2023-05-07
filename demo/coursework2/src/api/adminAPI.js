f91f2cb00f35a86b821ce14cd454f4d529b35b52
Boyu Chen <63703100+Travis-Chen00@users.noreply.github.com>
2023-05-08 05:27:32 +0800

    Delete demo/coursework2 directory

--- demo/coursework2/src/api/adminAPI.js
+++
@@ -1,6 +0,0 @@
-import axios from "../utils/request";
-
-export const $login =  async (params) =>{
-    let {data} =  axios.get('api/Login',  {params}).then()
-    console.log(data);
-} 
--- demo/coursework2/src/config/index.js
+++
@@ -1,2 +0,0 @@
-//配置文件
-export const baseURL = 'http://j3905s0414.qicp.vip:25458'
